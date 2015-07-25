afdca0b7586e83521eecf5c927d15a9b73bd6527
Tim Gasser <timgasser@hotmail.com>
2015-07-25 12:41:54 -0500

    Initial Commit

---
+++ DocOnDemand/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DocOnDemand
+//
+//  Created by Tim Gasser on 7/25/15.
+//  Copyright (c) 2015 timgsr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DocOnDemand/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DocOnDemand
+//
+//  Created by Tim Gasser on 7/25/15.
+//  Copyright (c) 2015 timgsr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
